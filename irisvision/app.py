793834b782cc3af80fe9e2f399e0fc9c54e7c3a5
pedrooliveira <pedrooliveira@rotadooeste.com.br>
2026-03-11 16:12:02 -0400

    feat: creating first routes to API

--- irisvision/app.py
+++ irisvision/app.py
@@ -1,8 +1,49 @@
-from fastapi import FastAPI
+import io
+from http import HTTPStatus
+
+import numpy as np
+from fastapi import FastAPI, UploadFile
+from PIL import Image
+from ultralytics import YOLO
+
+
+def load_model():
+    modelpath = r'models/best.pt'
+    model = YOLO(modelpath)
+
+    return model
+
+
+model = load_model()
 
 app = FastAPI()
 
+@app.get('/health', status_code=HTTPStatus.OK)
+def get_health():
+    return {'Health': 'Ok', 'Status Code': HTTPStatus.OK}
 
-@app.get('/helloworld')
-def hello_world():
-    return {'message': 'Hello, World!'}
+
+@app.post('/predict', status_code=HTTPStatus.OK)
+async def post_predictions(file: UploadFile):
+    image = await file.read()
+    image = Image.open(io.BytesIO(image))
+    result = model(image)
+    names = result[0].names
+    probability = result[0].probs.data.numpy()
+    prediction = np.argmax(probability)
+    response = {
+        'Prediction': names[prediction],
+        'Confidence': float(probability[prediction]),
+    }
+
+    return response
+
+
+@app.get('/classifications', status_code=HTTPStatus.OK)
+def get_classifications_list():
+    ...
+
+    
+@app.get('statistics', status_code=HTTPStatus.OK)
+def get_statistics():
+    ...
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,21 @@
+from http import HTTPStatus
+from pathlib import Path
+
 from fastapi.testclient import TestClient
 
 from irisvision.app import app
 
 
-def test_root_deve_retornar_ola_mundo():
+def test_json_post_predictions():
     client = TestClient(app)
 
-    response = client.get('/helloworld')
+    image_path = Path(__file__).parent / 'images' / 'test1.jpg'
+    image_bytes = image_path.read_bytes()
+    files = {'file': ('test1.jpg', image_bytes, 'image/jpeg')}
 
-    assert response.json() == {'message': 'Hello, World!'}
+    response = client.post('/predict', files=files)
+    assert response.status_code == HTTPStatus.OK
+    payload = response.json()
+
+    assert payload['Prediction']
+    assert payload['Confidence']
